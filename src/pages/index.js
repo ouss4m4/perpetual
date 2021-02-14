6475cb6b4d7398ec301a53fedd6efe55d4c4b4a2
o.baaziz <o.baaziz@pharmainvest.dz>
2021-02-14 17:25:41 +0100

    svg added

--- src/pages/index.js
+++ src/pages/index.js
@@ -81,7 +81,27 @@ export default () => (
         <h2 className="text-3xl lg:text-5xl font-semibold prime-color">Main Features</h2>
         <div className="flex flex-col lg:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
-            <Card className="mb-8" style={{ minHeight: '550px' }}>
+            <Card className="mb-8" style={{ minHeight: '800px' }}>
+              <svg
+                className="h-12 w-12 mx-auto mb-4"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
               <p className="font-semibold text-xl prime-color ">
                 {' '}
                 It Does the Heavy Lifting for You{' '}
@@ -102,7 +122,20 @@ export default () => (
             </Card>
           </div>
           <div className="flex-1 px-3">
-            <Card className="mb-8" style={{ minHeight: '550px' }}>
+            <Card className="mb-8" style={{ minHeight: '800px' }}>
+              <svg
+                className="h-12 w-12 mx-auto mb-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
+                  clipRule="evenodd"
+                />
+              </svg>
               <p className="font-semibold text-xl prime-color">Creates Residual Income</p>
               <p className="mt-4 text-lg font-semibold">
                 Perpetual Income 365 creates what is referred to as residual or monthly recurring
@@ -123,7 +156,20 @@ export default () => (
           </div>
 
           <div className="flex-1 px-3">
-            <Card className="mb-8" style={{ minHeight: '550px' }}>
+            <Card className="mb-8" style={{ minHeight: '800px' }}>
+              <svg
+                className="h-12 w-12 mx-auto mb-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M13 7H7v6h6V7z" />
+                <path
+                  fillRule="evenodd"
+                  d="M7 2a1 1 0 012 0v1h2V2a1 1 0 112 0v1h2a2 2 0 012 2v2h1a1 1 0 110 2h-1v2h1a1 1 0 110 2h-1v2a2 2 0 01-2 2h-2v1a1 1 0 11-2 0v-1H9v1a1 1 0 11-2 0v-1H5a2 2 0 01-2-2v-2H2a1 1 0 110-2h1V9H2a1 1 0 010-2h1V5a2 2 0 012-2h2V2zM5 5h10v10H5V5z"
+                  clipRule="evenodd"
+                />
+              </svg>
               <p className="font-semibold text-xl prime-color">It’s Fully Automated</p>
               <p className="mt-4 text-lg font-semibold">
                 This is perhaps Perpetual Income 365’s biggest timesaver.
